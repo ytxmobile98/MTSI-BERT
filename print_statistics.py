471c93eb1b7a4409b9aaff91169117dcd0de96a1
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2025-08-18 15:08:43 +0800

    chore: remove unused imports

--- print_statistics.py
+++ print_statistics.py
@@ -1,26 +1,16 @@
-import datetime
-import logging
-import os
-import pdb
-import sys
-
-import numpy as np
-import torch
 from transformers import BertTokenizer
-from torch.utils.data import DataLoader
-from torch import nn
 
-from model import (MTSIAdapterDataset, KvretConfig, KvretDataset, MTSIBert,
-                   MTSIKvretConfig, TwoSepTensorBuilder)
-
+from model import KvretConfig, KvretDataset
 
 
 def print_statistics_per_set(curr_set):
-    tokenizer = BertTokenizer.from_pretrained('bert-base-cased', do_lower_case = False)
+    tokenizer = BertTokenizer.from_pretrained(
+        'bert-base-cased', do_lower_case=False)
 
     tok, sentences, id = curr_set.get_max_tokens_per_dialogue(tokenizer)
     print('\n--- max tokens per dialogue ---')
-    print('num tokens = ' + str(tok) + ', \nnum sentences = ' + str(sentences) + ', \nid = ' + id)
+    print('num tokens = ' + str(tok) + ', \nnum sentences = ' +
+          str(sentences) + ', \nid = ' + id)
 
     tot_num = curr_set.get_total_user_utterances()
     print('\n--- total user utterances ---')
@@ -28,7 +18,8 @@ def print_statistics_per_set(curr_set):
 
     tok, sentences, id = curr_set.get_max_tokens_per_sentence(tokenizer)
     print('\n--- max tokens per sentence ---')
-    print('num tokens = ' + str(tok) + ', \nnum sentences = ' + str(sentences) + ', \nid = ' + id)
+    print('num tokens = ' + str(tok) + ', \nnum sentences = ' +
+          str(sentences) + ', \nid = ' + id)
 
     sentences, id = curr_set.get_max_utterances_per_dialogue()
     print('\n--- max sentences per dialogue ---')
@@ -43,21 +34,23 @@ def print_statistics_per_set(curr_set):
     print('\n--- check dialogues with subsequent same actor utterances ---')
     print('ids = ' + str(id_l))
     print('\n--- action frequency ---')
-    print('num fetch = ' + str(fetch_count) + ' vs num insert = ' + str(insert_count))
+    print('num fetch = ' + str(fetch_count) +
+          ' vs num insert = ' + str(insert_count))
 
 
 def statistics(remove_subsequent_actor=False):
     """
     To retrieve the max sentence lenth in order to apply the rigth amount of padding
     """
-    tokenizer = BertTokenizer.from_pretrained('bert-base-cased', do_lower_case = False)
+    tokenizer = BertTokenizer.from_pretrained(
+        'bert-base-cased', do_lower_case=False)
 
     print('\n\t\t\t### TRAINING ###')
     training_set = KvretDataset(KvretConfig._KVRET_TRAIN_PATH)
     if remove_subsequent_actor:
         training_set.remove_subsequent_actor_utterances()
     print_statistics_per_set(training_set)
-    
+
     print('\n\t\t\t### VALIDATION ###')
     validation_set = KvretDataset(KvretConfig._KVRET_VAL_PATH)
     if remove_subsequent_actor:
@@ -69,9 +62,6 @@ def statistics(remove_subsequent_actor=False):
     if remove_subsequent_actor:
         test_set.remove_subsequent_actor_utterances()
     print_statistics_per_set(test_set)
-
-
-
 
 
 if __name__ == '__main__':
