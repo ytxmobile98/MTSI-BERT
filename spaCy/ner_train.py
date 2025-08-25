e88fbc908c715f5e908bc3e3cd551894db3baf02
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2025-08-26 00:52:52 +0800

    test: make the training task runnable

--- spaCy/ner_train.py
+++ spaCy/ner_train.py
@@ -1,19 +1,14 @@
 import os
 import datetime
-import pdb
 import sys
-sys.path.insert(1, 'model/')
 import time
-
-from torch.utils.data import DataLoader
-
-from MTSIBertConfig import KvretConfig
+import random
+import spacy
+from spacy.training import Example
 from ner_kvret import NERKvretDataset
 
-import spacy
-import random
-
-
+sys.path.insert(1, 'model/')
+from MTSIBertConfig import KvretConfig
 
 
 _N_EPOCHS = 120
@@ -22,22 +17,19 @@ _BATCH_SIZE = 32
 
 
 def spacy_train(data):
-    
-
     nlp = spacy.blank('en')  # create blank Language class
     # create the built-in pipeline components and add them to the pipeline
     # nlp.create_pipe works for built-ins that are registered with spaCy
     if 'ner' not in nlp.pipe_names:
-        ner = nlp.create_pipe('ner')
-        nlp.add_pipe(ner, last=True)
+        nlp.add_pipe('ner')
+    ner = nlp.get_pipe('ner')
 
     # add labels
     for _, annotations in data:
-         for ent in annotations.get('entities'):
+        for ent in annotations.get('entities'):
             ner.add_label(ent[2])
 
-
- # get names of other pipes to disable them during training
+    # get names of other pipes to disable them during training
     other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'ner']
     with nlp.disable_pipes(*other_pipes):  # only train NER
         optimizer = nlp.begin_training()
@@ -51,13 +43,18 @@ def spacy_train(data):
                 if idx % _BATCH_SIZE == 0 or idx == len(data) - 1:
                     curr_batch_text.append(text)
                     curr_batch_label.append(annotations)
+                examples = [
+                    Example.from_dict(
+                        nlp.make_doc(text), annotations)
+                    for text, annotations in zip(
+                        curr_batch_text, curr_batch_label)
+                ]
                 nlp.update(
-                    curr_batch_text,  # batch of texts
-                    curr_batch_label,  # batch of annotations
+                    examples=examples,
                     drop=0.2,  # dropout
                     sgd=optimizer,  # callable to update weights
                     losses=losses)
-                
+
                 curr_batch_text = []
                 curr_batch_label = []
 
@@ -67,21 +64,12 @@ def spacy_train(data):
     return nlp
 
 
-
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
     start = time.time()
     curr_date = datetime.datetime.now().isoformat()
     # creates the directory for the checkpoints
     os.makedirs(os.path.dirname(_SPACY_MODEL_SAVING_PATH), exist_ok=True)
-    
+
     training_set = NERKvretDataset(KvretConfig._KVRET_TRAIN_PATH)
     straining_set = training_set.build_spacy_dataset()
 
@@ -90,8 +78,9 @@ if __name__ == '__main__':
 
     # train on both training and validation set
     spacy_model = spacy_train(straining_set+svalidation_set)
-    spacy_model.to_disk(_SPACY_MODEL_SAVING_PATH+'ner/'+curr_date)
-
+    save_dir = os.path.join(_SPACY_MODEL_SAVING_PATH, 'ner')
+    os.makedirs(save_dir, exist_ok=True)
+    spacy_model.to_disk(os.path.join(save_dir, curr_date))
 
     end = time.time()
     h_count = (end-start)/60/60
